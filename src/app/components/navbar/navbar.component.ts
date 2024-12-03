7e185b75433ad3dece15d2ba44197a961a9abdf4
GermanDiaz <germanrodrigo.2003@hotmail.com>
2024-12-03 11:55:01 -0500

    actualizano cli

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
